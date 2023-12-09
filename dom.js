fc53fd1737d3299f7a03c0dfd3cad0b568aaf57b
Your Name <you@example.com>
2023-12-09 15:02:13 +0530

    queary task

--- dom.js
+++ dom.js
@@ -1,23 +1,27 @@
-var ulElement = document.getElementById("items");
+var secondLi = document.querySelector("#items li:nth-child(2)");
 
-var newLiElement = document.createElement("li");
-
-newLiElement.textContent = "New Item";
-
-newLiElement.classList.add("editable-item");
-
-ulElement.appendChild(newLiElement);
-
-var editableItems = document.getElementsByClassName("editable-item");
-
-  if (editableItems.length > 0) {
-    editableItems[0].style.fontWeight = "bold";
-    editableItems[0].style.color = "green";
+  // Check if the element is found
+  if (secondLi) {
+    // Apply the green background color
+    secondLi.style.backgroundColor = "green";
   }
+var thirdLi = document.querySelector("#items li:nth-child(3)");
 
-  var liElements = ulElement.getElementsByTagName("li");
+  // Check if the element is found
+  if (thirdLi) {
+    // Make the third <li> element invisible
+    thirdLi.style.display = "none";
+  }
+var listItems = document.querySelectorAll("#items li");
 
-  if (liElements.length > 0) {
-    liElements[liElements.length - 1].style.fontWeight = "bold";
-    liElements[liElements.length - 1].style.color = "blue";
+  // Check if there are at least two elements
+  if (listItems.length >= 2) {
+    // Apply the green font color to the second <li> element
+    listItems[1].style.color = "green";
+  }
+var oddListItems = document.querySelectorAll("#items li:nth-child(odd)");
+
+  // Loop through the selected odd elements and apply the green background color
+  for (var i = 0; i < oddListItems.length; i++) {
+    oddListItems[i].style.backgroundColor = "green";
   }

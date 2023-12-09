ef970db30964b7f3e336508f416855055bc6ba70
Your Name <you@example.com>
2023-12-09 15:16:26 +0530

    using all keyword

--- dom.js
+++ dom.js
@@ -1,27 +1,53 @@
-var secondLi = document.querySelector("#items li:nth-child(2)");
+// Get the parent element
+var mainDiv = document.getElementById("main");
 
-  // Check if the element is found
-  if (secondLi) {
-    // Apply the green background color
-    secondLi.style.backgroundColor = "green";
-  }
-var thirdLi = document.querySelector("#items li:nth-child(3)");
+// Create a new text node with "Hello"
+var helloTextNode = document.createTextNode("Hello ");
 
-  // Check if the element is found
-  if (thirdLi) {
-    // Make the third <li> element invisible
-    thirdLi.style.display = "none";
-  }
-var listItems = document.querySelectorAll("#items li");
+// Insert the text node before "Item Lister"
+mainDiv.firstElementChild.insertBefore(helloTextNode, mainDiv.firstElementChild.lastChild.previousElementSibling);
 
-  // Check if there are at least two elements
-  if (listItems.length >= 2) {
-    // Apply the green font color to the second <li> element
-    listItems[1].style.color = "green";
-  }
-var oddListItems = document.querySelectorAll("#items li:nth-child(odd)");
+// Access the last child element
+var lastChild = mainDiv.lastElementChild;
 
-  // Loop through the selected odd elements and apply the green background color
-  for (var i = 0; i < oddListItems.length; i++) {
-    oddListItems[i].style.backgroundColor = "green";
-  }
+// Access the last child element using lastChild
+var lastChildAlt = mainDiv.lastChild;
+
+// Get the <ul> element with the ID 'items'
+var itemsList = document.getElementById("items");
+
+// Get the first <li> element
+var firstListItem = itemsList.firstElementChild;
+
+// Create a new text node with "Hello"
+var helloTextNode2 = document.createTextNode("Hello ");
+
+// Insert the text node before "Item 1"
+itemsList.insertBefore(helloTextNode2, firstListItem);
+
+// Access the next sibling of "Item 1"
+var nextSibling = firstListItem.nextSibling;
+
+// Access the next sibling element of "Item 1"
+var nextElementSibling = firstListItem.nextElementSibling;
+
+// Access the previous sibling of "Item 1"
+var previousSibling = firstListItem.previousSibling;
+
+// Access the previous sibling element of "Item 1"
+var previousElementSibling = firstListItem.previousElementSibling;
+
+// Create a new element
+var newElement = document.createElement("li");
+
+// Set an attribute on the new element
+newElement.setAttribute("class", "list-group-item");
+
+// Create a new text node for the new element
+var newText = document.createTextNode("New Item");
+
+// Append the text node to the new element
+newElement.appendChild(newText);
+
+// Append the new element to the existing list
+itemsList.appendChild(newElement);
